90ea363e1a54a41f1e0129fd64f6c902b3c2d653
Luigi Matias <igiboi26@gmail.com>
2022-11-08 16:59:50 -0500

    added pages and browser router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path="/quotes">
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <NewQuote />
+        </Route>
+        <Route path="/new-quote">
+          <QuoteDetail />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,9 @@
+const QuoteDetail = () => {
+  return (
+    <div>
+      <h1>Quote Detail</h1>
+    </div>
+  );
+};
+
+export default QuoteDetail;
