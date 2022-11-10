43d559a35ca20720882aa92e714225803aaf58a7
Luigi Matias <igiboi26@gmail.com>
2022-11-09 22:27:31 -0500

    added a comment section on quote detail

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,6 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const params = useParams();
@@ -7,6 +9,10 @@ const QuoteDetail = () => {
     <div>
       <h1>Quote Detail</h1>
       <p>{params.quoteId}</p>
+
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </div>
   );
 };
